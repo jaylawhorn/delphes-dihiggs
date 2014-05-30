56ae52c25afdc5b6b02e640dc4a6e4d79dd5429d
Jay <jlawhorn@mit.edu>
2014-05-30 15:59:42 +0200

    new version, better automated, with puID

--- Selection/master.sh
+++ Selection/master.sh
@@ -2,17 +2,15 @@
 
 #master run script for pre-processing
 
-conf_file=$1
-eos_switch=$2
-
-if [ ${eos_switch} -eq "1" ]; then
-    base_path="/store/group/phys_higgs/upgrade/PhaseII/Configuration4v2/140PileUp/"
-elif [ ${eos_switch} -eq "0" ]; then
-    base_path="/afs/cern.ch/work/j/jlawhorn/"
-else
-    echo "EOS toggle doesn't make sense. please try again"
+if [ ${#} -lt "1" ]; then
+    echo "Please use the following syntax: " 
+    echo "    ./master.sh <conf_file>"
+    echo " "
+    echo "<conf_file> is a text file containing the names of all samples"
     exit
 fi
+
+conf_file=$1
 
 while read line #loop over lines in ${conf_file}
 do
@@ -20,7 +18,11 @@ do
   if [ "${array[0]:0:1}" != "#" ]; then 
       echo "Preparing sample" ${array[0]}
       rm ${array[0]}.txt
-      filelist=(`/afs/cern.ch/project/eos/installation/0.3.4/bin/eos.select ls ${base_path}${array[0]}/`)
+      if [ ${array[2]} -eq "1" ]; then
+	  filelist=(`/afs/cern.ch/project/eos/installation/0.3.4/bin/eos.select ls ${array[3]} | grep ${array[0]}`)
+      elif [ ${array[2]} -eq "0" ]; then
+	  filelist=(`ls ${array[3]} | grep ${array[0]}`)
+      fi
       outarray=()
       for ((i=0; i<${#filelist[@]}; i++)) 
       do
@@ -28,16 +30,40 @@ do
 	      outarray+=(${filelist[$i]})
 	  fi
       done
-      echo ${#outarray[@]} "files found in" ${base_path}${array[0]}
+      echo ${#outarray[@]} "files found in" ${array[3]}
 
       for ((i=0; i<${#outarray[@]}; i++))
       do
-	  echo "root://eoscms.cern.ch/"${base_path}${array[0]}${outarray[i]} >> ${array[0]}.txt
+	  if [ ${array[2]} -eq "1" ]; then
+	      echo "root://eoscms.cern.ch/"${array[3]}"/"${outarray[i]} >> ${array[0]}.txt
+	  elif [ ${array[2]} -eq "0" ]; then
+	      echo ${array[3]}"/"${outarray[i]} >> ${array[0]}.txt
+	  fi
       done
 
       echo `wc -l ${array[0]}.txt | cut -d' ' -f1` "files listed in" ${array[0]}.txt
 
-      root -l -q -b getall.C+\(\"${array[0]}.txt\"\)
+      root -l -q -b getall.C+\(\"${array[0]}.txt\"\) | tail -1 > ${array[0]}_events.txt
+
+      echo `cat ${array[0]}_events.txt` "events found"
+
+      mkdir -p "/afs/cern.ch/work/j/jlawhorn/public/ntuples/"${array[0]}
+
+      n=0
+
+      rm ${array[0]}_run.sh
+      head -7 run_outline.sh > ${array[0]}_run.sh
+      while read line2
+      do
+	  echo root -l -q -b selection.C+\\\(\\\"${line2}\\\",${array[1]},`cat ${array[0]}_events.txt`,\\\""/afs/cern.ch/work/j/jlawhorn/public/ntuples/"${array[0]}/${n}".root"\\\"\\\) >> ${array[0]}_run.sh
+	  n=$((n+1))
+	      
+      done < ${array[0]}.txt
+
+      tail -3 run_outline.sh >> ${array[0]}_run.sh
+      chmod u+x ${array[0]}_run.sh
+
+      bsub -q 1nd ${array[0]}_run.sh
   fi
 done < ${conf_file}
 
